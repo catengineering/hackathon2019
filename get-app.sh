012f2cf869ba1afa352c53a3fc078d0d5dc7795d
johm-msft <johm@microsoft.com>
2019-10-03 09:25:37 -0700

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 size=""
 ulimit -n 20000
-while :
-do
-	/usr/bin/wget -O /opt/md5 https://raw.githubusercontent.com/catengineering/hackathon2019/master/md5
-	/usr/bin/wget -O /opt/server https://github.com/catengineering/hackathon2019/raw/master/server
-	sleep 5
-	md5=$(cat /opt/md5)
-	size=$(md5sum /opt/server | awk '{print $1}')
+/usr/bin/wget -O /opt/md5 https://raw.githubusercontent.com/catengineering/hackathon2019/master/md5
+/usr/bin/wget -O /opt/server https://github.com/catengineering/hackathon2019/raw/master/server
+sleep 5
+md5=$(cat /opt/md5)
+size=$(md5sum /opt/server | awk '{print $1}')
 
-	if [ "$size" = "$md5" ]; then
-		 chmod +x /opt/server
-		 /opt/server
-	else
-		rm -f /opt/server
-	fi
-done
+if [ "$size" = "$md5" ]; then
+   chmod +x /opt/server
+   /opt/server
+else
+   rm -f /opt/server
+fi
+
