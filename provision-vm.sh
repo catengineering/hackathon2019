96269942891abf733e2eaa2c8c8eb77406079d30
johm-msft <johm@microsoft.com>
2020-03-20 16:30:46 -0700

    Update provision-vm.sh

--- provision-vm.sh
+++ provision-vm.sh
@@ -9,9 +9,9 @@ chmod +x /opt/get-app.sh
 
 # configure the script to auto-run on reboot
 echo "#!/bin/sh -e" >/etc/rc.local
-echo /opt/get-app.sh >>/etc/rc.local
+echo /opt/get-app.sh 5400 14400 >>/etc/rc.local
 echo exit 0 >>/etc/rc.local
 chmod +x /etc/rc.local
 
 # start the app and leave running
-nohup /opt/get-app.sh &
+nohup /opt/get-app.sh 5400 14400 &
